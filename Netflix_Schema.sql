5236a6b4054d495121269c4b6c6de77aa3ee30a2
Mallikaa-24 <maheshkarmallika@gmail.com>
2025-05-15 14:43:27 +0200

    Add files via upload

---
+++ Netflix_Schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix
+(
+    show_id      VARCHAR(10),
+    type         VARCHAR(10),
+    title        VARCHAR(250),
+    director     VARCHAR(550),
+    casts        VARCHAR(1050),
+    country      VARCHAR(550),
+    date_added   VARCHAR(55),
+    release_year VARCHAR(100),
+    rating       VARCHAR(15),
+    duration     VARCHAR(15),
+    listed_in    VARCHAR(250),
+    description  VARCHAR(550)
+);
